5bbfc8a0dfb051260a6ccf6142cd478f67a325d8
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-06-03 22:36:42 +0300

    finish

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # goit-node-hw-01
+
+"list" -   https://ibb.co/THfZdqz
+
+"get"  -   https://ibb.co/gSyKPw3
+
+"add"  -   https://ibb.co/9H9g4jD
+
+"remove"-  https://ibb.co/6mHxRsw
+
--- contacts.js
+++ contacts.js
@@ -1,13 +1,15 @@
-const {readFile, writeFile} = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises;
 const path = require('path');
+const {nanoid} = require('nanoid');
 
-const contactsPath = path.join('db','contacts.json');
+
+const contactsPath = path.resolve('db','contacts.json');
 
 
 async function listContacts() {
     try {
       const readResult = await readFile(contactsPath);         
-      console.log(JSON.parse(readResult));
+      console.table(JSON.parse(readResult));
         
     } catch (err) {
       console.log(err);
@@ -21,7 +23,7 @@ async function getContactById(contactId) {
 
       contacts.find(contact => {
           if (contact.id === contactId) {
-            console.log(contact);
+            console.table(contact);
           }
       })
         
@@ -31,26 +33,40 @@ async function getContactById(contactId) {
 }
 
 async function removeContact(contactId) {
-  // try {
-  //     const readResult = await readFile(contactsPath);         
-  //     const contacts = JSON.parse(readResult);
+  try {
+      const readResult = await readFile(contactsPath);         
+      const contacts = JSON.parse(readResult);
 
-  //     contacts.find(contact => {
-  //         if (contact.id === contactId) {
-  //           console.log(contact);
-  //         }
-  //     })
-        
-  //   } catch (err) {
-  //     console.log(err);
-  //   } 
+      const newContacts = contacts.filter(contact => contact.id !== contactId)
+      console.table(newContacts);
+    
+    } catch (err) {
+      console.log(err);
+    } 
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  try {
+      const readResult = await readFile(contactsPath);         
+      const contacts = JSON.parse(readResult);
+     
+      const id = `${nanoid()}`;
+      const newContact = { id, name, email, phone };
+    
+      contacts.push(newContact);
+
+      await writeFile(contactsPath, JSON.stringify(contacts));      
+      console.table(contacts); 
+      
+    
+    } catch (err) {
+      console.log(err);
+    } 
 }
 
-
-// listContacts();
-// getContactById("rsKkOQUi80UsgVPCcLZZW");
-
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,  
+};
